efb1b1700d6d30288c07e14a0cbd11d02a222fc9
Katie Frey <katieefrey@gmail.com>
2022-02-01 20:26:27 -0500

    initial set up

---
+++ gpi/main/manage.py
@@ -0,0 +1,23 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^jyxa+2+qnay=dk8-!foz+6k69_$w(u0x5-w_pq!oc4rwr2^dx'
+
+
+# WOLBA.CH
+# DBLOC = "WOLBA.CH"
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'metasat',
+#         'HOST': 'mysql.wolba.ch',
+#         'PORT': '3306',
+#         'USER': 'metasat_db_user',  # read only account username
+#         'PASSWORD': '2Much2do!m3tasat',  # read only account password
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         },
+#     }
+# }
+
+
+
+# %Q&CW!vbGBGjUX&!#xO2^bT9XOQ7
